379b560b29e138a26278cd03d15122d78a7fe8e6
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2022-12-24 15:10:49 +0200

    Added project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './layouts/homepage/homepage.component';
+import { ShopComponent } from './layouts/shop/shop.component';
+import { ProductDetailComponent } from './layouts/product-detail/product-detail.component';
+import { ContactComponent } from './layouts/contact/contact.component';
+import { CheckoutComponent } from './layouts/checkout/checkout.component';
+import { CartComponent } from './layouts/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ShopComponent,
+    ProductDetailComponent,
+    ContactComponent,
+    CheckoutComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
