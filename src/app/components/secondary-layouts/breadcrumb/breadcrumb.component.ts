12fa73b243305299ed8d36fb039dd6738d53e5d8
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2023-01-21 00:44:19 +0200

    Added box shadow to searchBar on hover

--- src/app/components/secondary-layouts/breadcrumb/breadcrumb.component.ts
+++ src/app/components/secondary-layouts/breadcrumb/breadcrumb.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.scss']
 })
-export class BreadcrumbComponent {
-
+export class BreadcrumbComponent implements OnInit{
+constructor(private _router: Router, private route: ActivatedRoute){}
+ngOnInit(): void {
+    
+}
 }
