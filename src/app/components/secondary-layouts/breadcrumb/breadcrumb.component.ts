dae086f8e787001c1d940a3e525ca9f076855f63
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2023-02-06 09:04:21 +0200

    fixed breadcrumbs when searching for a product

--- src/app/components/secondary-layouts/breadcrumb/breadcrumb.component.ts
+++ src/app/components/secondary-layouts/breadcrumb/breadcrumb.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductserviceService } from 'src/app/services/product/productservice.service';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -10,14 +11,20 @@ export class BreadcrumbComponent implements OnInit{
   currentRoute: string = '';
   currentKey: string = '';
   currentParams: string = '';
-constructor(private _router: Router, private route: ActivatedRoute){}
+
+constructor(private _router: Router, private route: ActivatedRoute, private productService: ProductserviceService){}
   // Getting Current Searched/Selected on Load.
   ngOnInit(): void {
     this.currentRoute = this._router.url.split('?')[0];
     this.currentRoute = this.currentRoute.replace('/','')
-    this.route.queryParamMap.subscribe(params =>{
+    this.route.queryParamMap.subscribe(async params =>{
       this.currentKey = params.keys[0]
       this.currentParams = params['params'][this.currentKey]
+      if(Number(this.currentParams)) {
+        ( await this.productService.getProductByID(Number(this.currentParams))).subscribe((data:object) => {
+          this.currentParams = data['title']
+        })
+      }
     })
   }
 }
